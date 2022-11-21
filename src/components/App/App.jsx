06687783728e9bba6ba3b6b667b9565b5f57d42b
Yulia Sukhonenko <103435385+Yulia0907@users.noreply.github.com>
2022-11-21 20:10:31 +0200

    коректировка фильтра

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,12 @@ import { ContactList } from '../ContactList/ContactList';
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
   });
   const [filter, setFilter] = useState('');
 
